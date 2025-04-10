1994b07e8af2fef268f3bc957799b061edb3cb2e
Shashi prakash singh <155473349+Singhshashi18@users.noreply.github.com>
2025-04-10 10:48:41 +0530

    Update LoginPage.jsx

--- frontend/CHAT-APP/src/pages/LoginPage.jsx
+++ frontend/CHAT-APP/src/pages/LoginPage.jsx
@@ -1,15 +1,3 @@
-// import React from 'react'
-
-// const LoginPage = () => {
-//   return (
-//     <div>
-      
-//     </div>
-//   )
-// }
-
-// export default LoginPage
-
 
 import React from "react";
 import { useState } from "react";
