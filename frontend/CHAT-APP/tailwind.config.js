768da4d00e81645b5f51bbc842ac15a949b41a16
Shashi prakash singh <singhshashi09871@gmail.com>
2025-04-11 23:18:49 +0530

    Update tailwind.config.js

--- frontend/CHAT-APP/tailwind.config.js
+++ frontend/CHAT-APP/tailwind.config.js
@@ -2,7 +2,7 @@ import daisyui from "daisyui";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src//*.{js,jsx,ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
@@ -15,6 +15,6 @@ export default {
       "wireframe", "black", "luxury", "dracula", "cmyk", "autumn",
       "business", "acid", "lemonade", "night", "coffee", "winter",
       "dim", "nord"
-    ],
-  },
+    ],
+  },
 };
